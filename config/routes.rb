23a1a9eb9c5e3f673bd4d92053a4ea907b32b11f
The-Flying-Dev <leandranm@outlook.com>
2022-08-16 18:28:12 -0700

    Add routes and spinner

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  namespace :api do 
-    namespace :v1 do
-      resources :stocks, exept: [:show, :new, :edit]
-    end    
-  end
+  resources :jobs, only: :create 
+  resources :locations, only: :index 
+  resources :tradesman
   
   root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
