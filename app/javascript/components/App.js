9a1600189bb69be776174ee7fc06c5e540f5a8f2
The-Flying-Dev <leandranm@outlook.com>
2022-08-16 19:21:44 -0700

    Add App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Dashboard from './Dashboard';
+import TradesmanDetails from './TradesmanDetails';
 
 function App() {
   return (
-    <div className="container">
-        <div className="jumbotron">
-          <h1>ReactJS</h1>
-          <p>Test</p>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            
-          </div>
-        </div>
-      </div>
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route index element={<Dashboard />} /> {/* using index, as dashboard is a child route  with no path, it renders at the App's url */}
+          <Route path='/tradesmen/:id/*' element={<TradesmanDetails />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from '../components/App';
@@ -7,9 +8,11 @@ import App from '../components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
     document.body.appendChild(document.createElement('div')),
-  )
-})
+  );
+});
 
 
